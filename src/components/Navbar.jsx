cd667923c16dba036db79ea63b40ff71b85878d8
AbdElRahman <xinfargo@gmail.com>
2026-06-28 15:33:03 +0300

    Add Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,24 @@ import '../index.css'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+   <nav>
+    <div className="app-container flex justify-between mt-2 border-b border-gray-200 pb-5">
+      {/*left nav*/}
+      <div className='gradient-text flex items-center'>
+        Filimo
+      </div>
+      {/*middle nav*/}
+      <div className='flex items-center gap-4 md:gap-6 lg:gap-8'>
+        <span className='mspan'>Explore</span>
+        <span className='mspan'>Top Rated</span>
+        <span className='mspan'>My list</span>
+      </div>
+      {/*right nav*/}
+      <div className='flex items-center'>
+        <button className='border border-gray-300 px-6 py-1 rounded-2xl active:bg-gray-500 active:text-white'>Sign In</button>
+      </div>
+    </div>
+   </nav>
   )
 }
 
